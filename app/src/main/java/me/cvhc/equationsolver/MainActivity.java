a9464688203ba5023d4b6256832f42974950e3e1
CUI Hao <cuihao.leo@gmail.com>
2016-01-31 23:43:22 +0800

    update code in MainActivity.java

--- app/src/main/java/me/cvhc/equationsolver/MainActivity.java
+++ app/src/main/java/me/cvhc/equationsolver/MainActivity.java
@@ -1,52 +1,82 @@
-package me.cvhc.myapplication;
+package me.cvhc.equationsolver;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewEquation;
+    ListView listViewVariables;
+    ArrayAdapter<String> adapterVariables;
+    ArrayList<String> listVariables = new ArrayList<String>();
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        textViewEquation = (TextView)findViewById(R.id.textViewEquation);
+        listViewVariables = (ListView)findViewById(R.id.listViewVariables);
+
+        adapterVariables = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, listVariables);
+        listViewVariables.setAdapter(adapterVariables);
+
+        textViewEquation.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String eq = s.toString();
+                String[] part = eq.split("=", 2);
+
+                Log.d(LOG_TAG, "Equation: " + eq);
+                if (part.length != 2) {
+                    return;
+                }
+
+                ExpressionEvaluator left = new ExpressionEvaluator(part[0]);
+                ExpressionEvaluator right = new ExpressionEvaluator(part[1]);
+
+                Log.d(LOG_TAG, "left part: " + part[0]);
+                Log.d(LOG_TAG, "right part: " + part[1]);
+
+                if (left.isError() || right.isError()) {
+                    textViewEquation.setBackgroundResource(R.color.red);
+                }
+                else {
+                    textViewEquation.setBackgroundResource(android.R.color.transparent);
+
+                    listVariables.clear();
+                    for (Character c: left.getProperty().Variables) {
+                        listVariables.add(c.toString());
+                    }
+                    for (Character c: right.getProperty().Variables) {
+                        listVariables.add(c.toString());
+                    }
+
+                    adapterVariables.notifyDataSetChanged();
+
+                }
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
