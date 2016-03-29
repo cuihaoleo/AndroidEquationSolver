1d04b2108406a47746b9494bbf69cbe4975be70b
CUI Hao <cuihao.leo@gmail.com>
2016-03-29 11:09:20 +0800

    Implement custom keyboard

---
+++ app/src/main/java/me/cvhc/equationsolver/ExpressionKeypad.java
@@ -0,0 +1,16 @@
+package me.cvhc.equationsolver;
+
+import android.content.Context;
+import android.inputmethodservice.KeyboardView;
+import android.util.AttributeSet;
+
+
+public class ExpressionKeypad extends KeyboardView {
+    public ExpressionKeypad(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ExpressionKeypad(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
