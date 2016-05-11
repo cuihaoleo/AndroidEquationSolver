08c0b3a1e89e8678d365eb2a24a54ccd94955af9
CUI Hao <cuihao.leo@gmail.com>
2016-05-11 16:11:36 +0800

    Remove spinner in MainActivity's actionbar

--- app/src/main/java/me/cvhc/equationsolver/MainActivity.java
+++ app/src/main/java/me/cvhc/equationsolver/MainActivity.java
@@ -3,25 +3,12 @@ package me.cvhc.equationsolver;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
-import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.content.Context;
-import android.support.v7.widget.ThemedSpinnerAdapter;
-import android.content.res.Resources.Theme;
-
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     MainFragment mCurrentFragment;
@@ -37,28 +24,12 @@ public class MainActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         assert actionBar != null;
-        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(true);
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        assert spinner != null;
-        spinner.setAdapter(new MyAdapter(
-                toolbar.getContext(),
-                new String[]{
-                        "Bisection",
-                        "Todo",  // TODO: Implement another solving method
-                }));
-
-        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                mCurrentFragment = MainFragment.newInstance(position + 1);
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.container, mCurrentFragment)
-                        .commit();
-            }
-
-            @Override public void onNothingSelected(AdapterView<?> parent) { }
-        });
+        mCurrentFragment = MainFragment.newInstance(0);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, mCurrentFragment)
+                .commit();
     }
 
     /**
@@ -104,43 +75,6 @@ public class MainActivity extends AppCompatActivity {
                         .show();
             default:
                 return super.onOptionsItemSelected(item);
-        }
-    }
-
-
-    private static class MyAdapter extends ArrayAdapter<String> implements ThemedSpinnerAdapter {
-        private final ThemedSpinnerAdapter.Helper mDropDownHelper;
-
-        public MyAdapter(Context context, String[] objects) {
-            super(context, android.R.layout.simple_list_item_1, objects);
-            mDropDownHelper = new ThemedSpinnerAdapter.Helper(context);
-        }
-
-        @Override
-        public View getDropDownView(int position, View convertView, ViewGroup parent) {
-            View view;
-
-            if (convertView == null) {
-                LayoutInflater inflater = mDropDownHelper.getDropDownViewInflater();
-                view = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
-            } else {
-                view = convertView;
-            }
-
-            TextView textView = (TextView) view.findViewById(android.R.id.text1);
-            textView.setText(getItem(position));
-
-            return view;
-        }
-
-        @Override
-        public void setDropDownViewTheme(Theme theme) {
-            mDropDownHelper.setDropDownViewTheme(theme);
-        }
-
-        @Override
-        public Theme getDropDownViewTheme() {
-            return mDropDownHelper.getDropDownViewTheme();
         }
     }
 }
