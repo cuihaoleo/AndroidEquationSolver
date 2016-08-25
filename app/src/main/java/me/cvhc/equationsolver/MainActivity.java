29ae0548eeed6aa551829cc4b0009c27acc76d7b
CUI Hao <cuihao.leo@gmail.com>
2016-08-25 11:30:54 +0800

    Implement IMEDetectActivity which has a listener for softkeyboard (dis)appear

--- app/src/main/java/me/cvhc/equationsolver/MainActivity.java
+++ app/src/main/java/me/cvhc/equationsolver/MainActivity.java
@@ -5,12 +5,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends IMEDetectActivity {
     MainFragment mCurrentFragment;
 
     @Override
