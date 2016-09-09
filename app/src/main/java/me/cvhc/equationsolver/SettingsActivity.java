e3453fb3201c47a63e10763a86fa9945a2bd6c05
CUI Hao <cuihao.leo@gmail.com>
2016-09-09 14:29:46 +0800

    把 About 移动到设置页面，并完善内容

--- app/src/main/java/me/cvhc/equationsolver/SettingsActivity.java
+++ app/src/main/java/me/cvhc/equationsolver/SettingsActivity.java
@@ -1,9 +1,12 @@
 package me.cvhc.equationsolver;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
@@ -11,7 +14,11 @@ import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class SettingsActivity extends AppCompatPreferenceActivity {
@@ -59,6 +66,16 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                     return true;
                 }
             });
+
+            Preference prefAbout = findPreference("pref_about");
+            prefAbout.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    showAbout(getActivity());
+                    return true;
+                }
+            });
+
 
             initPrefSummary();
         }
@@ -110,6 +127,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                     break;
                 case "pref_default_lower_bound":
                 case "pref_default_upper_bound":
+                case "pref_default_bingo":
                     pref.setSummary(String.format(getString(R.string.format_bound),
                             sharedPreferences.getFloat(key, Float.NaN)));
                     break;
@@ -135,5 +153,60 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             return true;
         }
         return super.onMenuItemSelected(featureId, item);
+    }
+
+    private static void showAbout(Context context) {
+        final String APP_WEBSITE = "http://home.ustc.edu.cn/~cvhc/EquationSolver";
+        final String APP_EMAIL = "cuihao.leo+equationsolver@gmail.com";
+        final String APP_INTRO =
+                                "An Android app for equation solving.<br>" +
+                                "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
+                                "written by <a href=\"http://i-yu.me/\">CUI Hao</a><br>" +
+                                "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
+                                "directed by <a href=\"http://staff.ustc.edu.cn/~lshao/\">Limin Shao</a>";
+
+        AlertDialog.Builder aboutDialog = new AlertDialog.Builder(context);
+        aboutDialog.setTitle(getApplicationName(context));
+        aboutDialog.setIcon(R.mipmap.ic_launcher);
+        aboutDialog.setMessage(Html.fromHtml(APP_INTRO));
+
+        final Context contextCopy = context;
+        aboutDialog.setNegativeButton("Report Bug", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("message/rfc822");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{APP_EMAIL});
+                intent.putExtra(Intent.EXTRA_SUBJECT, "[Equation Solver] Report Bug");
+                intent.putExtra(Intent.EXTRA_TEXT, "");
+
+                try {
+                    contextCopy.startActivity(Intent.createChooser(intent, "Send mail..."));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(contextCopy, "No email clients installed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        aboutDialog.setPositiveButton("Website", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(APP_WEBSITE));
+                contextCopy.startActivity(browserIntent);
+            }
+        });
+
+        aboutDialog.setNeutralButton(android.R.string.ok, null);
+
+        Dialog dialog = aboutDialog.create();
+        dialog.show();
+
+        TextView message = (TextView)dialog.findViewById(android.R.id.message);
+        message.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    public static String getApplicationName(Context context) {
+        int stringId = context.getApplicationInfo().labelRes;
+        return context.getString(stringId);
     }
 }
